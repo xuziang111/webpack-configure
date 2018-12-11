fc489a71099ca46469b41f843db426d7bd8abc47
xuziang111 <584788096@qq.com>
2018-12-11 20:18:37 +0800

    上传配置

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+      path: __dirname + "/public",//打包后的文件存放的地方
+      filename: "bundle.js"//打包后输出文件的文件名
+    },
+    mode:"development",
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true,//实时刷新
+        port:8888
+      },
+      module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env", "@babel/preset-react"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","postcss-loader"]
+                  })
+      
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader",
+                        "postcss-loader" // compiles Sass to CSS, using Node Sass by default
+                    ]
+                  })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+      ]
+  }
